0acb262f07380f40bc7435b383664cf7f216c6a7
Aurelio <aurelio.arenella@gmail.com>
2025-10-15 17:50:50 +0200

    add gracefull shotdown

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,20 @@ import app from './app';
 import config from './config/config';
 import logger from './shared/logger';
 
-app.listen(config.PORT, () => {
+const server = app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
+});
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM signal received: closing HTTP server');
+  server.close(() => {
+    logger.info('HTTP server closed');
+  });
+});
+
+process.on('SIGINT', () => {
+  logger.info('SIGINT signal received: closing HTTP server');
+  server.close(() => {
+    logger.info('HTTP server closed');
+  });
 });

01ad05c86c90cb20fe1479446585f87d32d41f2d
Aurelio <aurelio.arenella@gmail.com>
2025-10-15 17:24:13 +0200

    add shared logger

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import app from './app';
 import config from './config/config';
+import logger from './shared/logger';
 
 app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`);
+  logger.info(`Server running on port ${config.PORT}`);
 });
